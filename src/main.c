3eba725a002125e555efdbdc0ffde41fc3a29981
ifcaro <ifcaro@localhost>
2009-10-02 18:40:33 +0200

    Updated interface with dynamic menu

--- src/main.c
+++ src/main.c
@@ -6,98 +6,162 @@ extern int size_usbd_irx;
 extern void *usbhdfsd_irx;
 extern int size_usbhdfsd_irx;
 
-void LoadGameList(){
+// Submenu items variant of the game list... keeped in sync with the game item list (firstgame, actualgame)
+struct TSubMenuList* usb_submenu= NULL;
+struct TMenuItem usb_games_item;
+
+void ClearUSBSubMenu() {
+	DestroySubMenu(&usb_submenu);
+	AppendSubMenu(&usb_submenu, &disc_icon, "No Items", -1);
+	usb_games_item.submenu = usb_submenu;
+	usb_games_item.current = usb_submenu;
+}
+
+void RefreshUSBGameList() {
 	int fd, size, n;
 	TGameList *list;
 	TGame buffer;
 
-	if(usbdelay>100){
-	fd=fioOpen("mass:ul.cfg", O_RDONLY);
-	if(fd>=0){
-		if (max_games==0){
-			size = fioLseek(fd, 0, SEEK_END);  
-			fioLseek(fd, 0, SEEK_SET);  	
-			for(n=0;n<size;n+=0x40){
-				fioRead(fd, &buffer, 0x40);
-				list=(TGameList*)malloc(sizeof(TGameList));
-				memcpy(&list->Game,&buffer,sizeof(buffer));
-				if(actualgame!=NULL){
-					actualgame->next=list;
-					list->prev=actualgame;
-				}else{
-					firstgame=list;
-					list->prev=0;
+	if(usbdelay>100) {
+		usbdelay = 0;
+		fd=fioOpen("mass:ul.cfg", O_RDONLY);
+		if(fd>=0) {
+			if (max_games==0) {
+				DestroySubMenu(&usb_submenu);
+				usb_submenu = NULL;
+				usb_games_item.submenu = usb_submenu;
+				usb_games_item.current = usb_submenu;
+
+				size = fioLseek(fd, 0, SEEK_END);  
+				fioLseek(fd, 0, SEEK_SET); 
+				
+				// TODO: decide if reload is necessary or not based on the size or date of the file...
+				
+				int id = 1; // game id (or order)
+				for(n=0;n<size;n+=0x40,++id){
+					fioRead(fd, &buffer, 0x40);
+					list=(TGameList*)malloc(sizeof(TGameList));
+					memcpy(&list->Game,&buffer,sizeof(TGame));
+					if(actualgame!=NULL){
+						actualgame->next=list;
+						list->prev=actualgame;
+					}else{
+						firstgame=list;
+						list->prev=0;
+					}
+					list->next=0;
+					actualgame=list;
+					max_games++;
+					
+					AppendSubMenu(&usb_submenu, &disc_icon, actualgame->Game.Name, id);
 				}
-				list->next=0;
-				actualgame=list;
-				max_games++;
+				
+					
+				usb_games_item.submenu = usb_submenu;
+				usb_games_item.current = usb_submenu;
+				actualgame=firstgame;
 			}
-			actualgame=firstgame;
-		}
-	}else{
-		if(max_games>0){
-			actualgame=firstgame;
-			while(actualgame->next!=0){
-				actualgame=actualgame->next;
-				free(actualgame->prev);
+		} else {
+			if(max_games>0){
+				actualgame=firstgame;
+				while(actualgame->next!=0){
+					actualgame=actualgame->next;
+					free(actualgame->prev);
+				}
+				free(actualgame);
 			}
-			free(actualgame);
+			max_games=0;
+			
+			ClearUSBSubMenu();
 		}
-		max_games=0;
-	}
-	fioClose(fd);
-	usbdelay=0;
-	}else{
+		fioClose(fd);
+	} else {
 		usbdelay++;
 	}
 }
 
-void PrevGame(){
+// --------------------- Menu item callbacks --------------------
+void PrevUSBGame(struct TMenuItem *self) {
+	if (!actualgame)
+		return;
+	
 	if(actualgame->prev!=NULL){
 		actualgame=actualgame->prev;
 	}
 }
 
-void NextGame(){
+void NextUSBGame(struct TMenuItem *self) {
+	if (!actualgame)
+		return;
+
 	if(actualgame->next!=NULL){
 		actualgame=actualgame->next;
 	}
 }
 
+int ResetUSBOrder(struct TMenuItem *self) {
+	actualgame = firstgame;
+	return 0;
+}
+
+void ExecUSBGameSelection(struct TMenuItem* self, int id) {
+	if (id == -1)
+		return;
+	
+	padPortClose(0, 0);
+	padPortClose(1, 0);
+	padReset();
+	LaunchGame(&actualgame->Game);
+}
+
+// --------------------- Menu initialization --------------------
+struct TMenuItem usb_games_item  = {
+	&games_icon, "USB Games", NULL, NULL, NULL, &ExecUSBGameSelection, &NextUSBGame, &PrevUSBGame, &ResetUSBOrder, &RefreshUSBGameList
+};
+
+struct TMenuItem hdd_games_item = {
+	&games_icon, "HDD Games", NULL, NULL, NULL
+};
+
+struct TMenuItem network_games_item = {
+	&games_icon, "Network Games", NULL, NULL, NULL
+};
+
+struct TMenuItem apps_item = {
+	&games_icon, "Apps", NULL, NULL, NULL
+};
+
+void InitMenuItems() {
+	ClearUSBSubMenu();
+	
+	AppendMenuItem(&usb_games_item);
+	AppendMenuItem(&hdd_games_item);
+	// TODO: For example: AppendMenuItem(&network_games_item);
+	AppendMenuItem(&apps_item);
+}
+
+// --------------------- Main --------------------
 int main(void)
 {
-	int ret, id;
-	init_scr();
-	scr_printf("\tOPEN USB LOADER Iniciado...\n\n");
-
-	scr_printf("\tReset...\n");
 	Reset();
-
-	//InitGFX();
-
-	ret=SifLoadModule("rom0:SIO2MAN",0,0);
-	scr_printf("\trom0:SIO2MAN id=%d\n", ret);
-	ret=SifLoadModule("rom0:MCMAN",0,0);
-	scr_printf("\trom0:MCMAN id=%d\n", ret);
-	ret=SifLoadModule("rom0:PADMAN",0,0);
-	scr_printf("\trom0:PADMAN id=%d\n", ret);
+	InitGFX();
 	
-	scr_printf("\tSifInitRpc...\n");
+	SifLoadModule("rom0:SIO2MAN",0,0);
+	SifLoadModule("rom0:MCMAN",0,0);
+	SifLoadModule("rom0:PADMAN",0,0);
+	
 	SifInitRpc(0);
 	
-	id=SifExecModuleBuffer(&usbd_irx, size_usbd_irx, 0, NULL, &ret);
-	scr_printf("\tUSBD.IRX id=%d ret=%d\n", id, ret);
-	delay(3);
-	id=SifExecModuleBuffer(&usbhdfsd_irx, size_usbhdfsd_irx, 0, NULL, &ret);
-	scr_printf("\tUSBHDFSD id=%d ret=%d\n", id,  ret);
+	int ret, id;
 	
-	scr_printf("\tStartPad...\n");
+	id=SifExecModuleBuffer(&usbd_irx, size_usbd_irx, 0, NULL, &ret);
+	id=SifExecModuleBuffer(&usbhdfsd_irx, size_usbhdfsd_irx, 0, NULL, &ret);
+
+	/// Init custom menu items
+	InitMenuItems();
+	
 	StartPad();
 
-	delay(20);
-
-InitGFX();
-	
 	Intro();
 
 	max_games=0;
@@ -105,76 +169,34 @@ InitGFX();
 	frame=0;
 	TextColor(0x80,0x80,0x80,0x80);
 	
+	// first column is games
+	MenuNextH();
+	MenuNextH();
+	
 	while(1)
 	{
 		ReadPad();
-				
-		DrawBackground();
-			
-		DrawIcons();
-		
-		DrawInfo();
+
+		DrawScreen();
 				
 		if(GetKey(KEY_LEFT)){
-			if(selected_h>0){
-				h_anim=0;
-				selected_h--;
-				selected_v=0;
-				actualgame=firstgame;
-				v_anim=100;
-				direc=1;
-			}
+			MenuPrevH();
 		}else if(GetKey(KEY_RIGHT)){
-			if(selected_h<4){
-				h_anim=200;
-				selected_h++;
-				selected_v=0;
-				actualgame=firstgame;
-				v_anim=100;
-				direc=3;
-			}
+			MenuNextH();
 		}else if(GetKey(KEY_UP)){
-			if(selected_v>0){
-				v_anim=0;
-				selected_v--;
-				direc=4;
-				if(selected_h==2){
-					PrevGame();
-				}
-			}
+			MenuPrevV();
 		}else if(GetKey(KEY_DOWN)){
-			if(selected_v<max_v-1){
-				v_anim=200;
-				selected_v++;
-				direc=2;
-				if(selected_h==2){
-					NextGame();
-				}
-			}
+			MenuNextV();
 		}else if(GetKey(KEY_CROSS)){
-			if(selected_h==0){
-					__asm__ __volatile__(
-					"	li $3, 0x04;"
-					"	syscall;"
-					"	nop;"
-				);
-			}else if(selected_h==1){
-				if(selected_v==0){
-					DrawConfig();
-				}else{
-					MsgBox();
-				}
-			}else if(selected_h==2 && max_games>0){
-				padPortClose(0, 0);
-				padPortClose(1, 0);
-				padReset();
-				LaunchGame(&actualgame->Game);
-			}
+			// handle via callback in the menuitem
+			MenuItemExecute();
+		} else if(GetKey(KEY_SELECT)){
+			SwapMenu();
 		}
+
 		Flip();
-		if(selected_h==2){
-			LoadGameList();
-		}
+		
+		RefreshSubMenu();
 	}
 
 	return 0;

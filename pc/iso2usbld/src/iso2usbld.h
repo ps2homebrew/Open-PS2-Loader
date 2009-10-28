137b275101c498e896805a040bc90fb860076bce
jimmikaelkael <jimmikaelkael@users.noreply.github.com>
2009-10-28 16:48:15 +0100

    renamed PUBLIC share to PS2SMB

--- pc/iso2usbld/src/iso2usbld.h
+++ pc/iso2usbld/src/iso2usbld.h
@@ -24,9 +24,11 @@
 typedef	unsigned char 		u8;
 typedef unsigned short 		u16;
 typedef unsigned int		u32;
+typedef unsigned long		u64;
 typedef signed char 		s8;
 typedef signed short 		s16;
-typedef signed int		s32;
+typedef signed int			s32;
+typedef signed long			s64;
 
 typedef struct {
 	char name[32];

ce2365b8abcdfea5b717e185d73dd16477cfa82e
Michaël Jimenez <jimmikaelkael@wanadoo.fr>
2009-10-30 22:50:27 +0100

    iso2usbld: only use long long and not __int64

--- pc/iso2usbld/src/iso2usbld.h
+++ pc/iso2usbld/src/iso2usbld.h
@@ -25,19 +25,11 @@
 typedef	unsigned char 		u8;
 typedef unsigned short 		u16;
 typedef unsigned int		u32;
-#ifdef _WIN32
-typedef unsigned __int64	u64;
-#else
 typedef unsigned long long	u64;
-#endif
 typedef signed char 		s8;
 typedef signed short 		s16;
 typedef signed int		s32;
-#ifdef _WIN32
-typedef signed __int64		s64;
-#else
 typedef signed long long	s64;
-#endif
 
 typedef struct {
 	char name[32];

3db088417a826d8e15c34a26c196b1ef6131d424
jimmikaelkael <jimmikaelkael@users.noreply.github.com>
2009-11-04 18:12:29 +0100

    reverting back isofs to rev71

--- modules/isofs/isofs.c
+++ modules/isofs/isofs.c
@@ -1303,10 +1303,6 @@ int isofs_Open(const char *filename, int mode)
 		if (fh->status == 0)
 			break;
 	}
-
-	// check mode
-	if (mode != O_RDONLY)
-		return -2;	
 			
 	// too much files opened
 	if (fd == MAX_FDHANDLES)

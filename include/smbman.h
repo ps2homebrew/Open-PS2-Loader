e211cfd1d4089a32aaf6791f14508788cfbdd650
jimmikaelkael <jimmikaelkael@users.noreply.github.com>
2010-05-27 15:13:17 +0200

    smbman: removed connect/disconnect (now done by logon/logoff)

--- include/smbman.h
+++ include/smbman.h
@@ -12,7 +12,6 @@
 #define HASHED_PASSWORD		 1
 
 // DEVCTL commands
-#define SMB_DEVCTL_CONNECT		0xC0DE0000
 #define SMB_DEVCTL_GETPASSWORDHASHES	0xC0DE0001
 #define SMB_DEVCTL_LOGON		0xC0DE0002
 #define SMB_DEVCTL_LOGOFF		0xC0DE0003
@@ -21,14 +20,8 @@
 #define SMB_DEVCTL_CLOSESHARE		0xC0DE0006
 #define SMB_DEVCTL_ECHO			0xC0DE0007
 #define SMB_DEVCTL_QUERYDISKINFO	0xC0DE0008
-#define SMB_DEVCTL_DISCONNECT		0xC0DE0009
 
 // helpers for DEVCTL commands
-
-typedef struct {		// size = 20
-	char serverIP[16];
-	int  serverPort;
-} smbConnect_in_t;
 
 typedef struct {
 	char password[256];
@@ -40,6 +33,8 @@ typedef struct {		// size = 32
 } smbGetPasswordHashes_out_t;
 
 typedef struct {		// size = 536
+	char serverIP[16];
+	int  serverPort;
 	char User[256];
 	char Password[256];
 	int  PasswordType;	// PLAINTEXT_PASSWORD or HASHED_PASSWORD

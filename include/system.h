89cea7820898acbe770131ec680a836cb05999bf
jimmikaelkael <jimmikaelkael@localhost>
2010-05-29 18:03:54 +0200

    forgot system.h, now added

---
+++ include/system.h
@@ -0,0 +1,16 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+void delay(int count);
+unsigned int USBA_crc32(char *string);
+int sysGetDiscID(char *discID);
+void sysReset(void);
+void sysPowerOff(void);
+int sysPcmciaCheck(void);
+void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, int compatflags, int alt_ee_core);
+int sysExecElf(char *path, int argc, char **argv);
+int sysPS3Detect(void);
+int sysSetIPConfig(char* ipconfig);
+int sysLoadModuleBuffer(void *buffer, int size, int argc, char *argv);
+
+#endif

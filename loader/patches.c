39af16031cc388f9425ee7ed792414f4bee58d80
Michaël Jimenez <jimmikaelkael@wanadoo.fr>
2010-06-27 18:53:00 +0200

    Max Payne US version (SLUS_202.30) fix for SMB use

--- loader/patches.c
+++ loader/patches.c
@@ -20,7 +20,7 @@ typedef struct {
 	game_patch_t patch;
 } patchlist_t;
 
-static patchlist_t patch_list[14] = {
+static patchlist_t patch_list[15] = {
 	{ "SLES_524.58", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness PAL - disable cdvd timeout stuff
 	{ "SLUS_206.66", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness NTSC U - disable cdvd timeout stuff
 	{ "SLPS_202.51", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Makai Senki Disgaea NTSC J - disable cdvd timeout stuff
@@ -34,7 +34,8 @@ static patchlist_t patch_list[14] = {
 	{ "SLUS_212.00", USB_MODE, { 0xdeadbee1, 0x00000000, 0x00000000 }}, // Armored Core Nine Breaker NTSC U - skip failing case on binding a RPC server
 	{ "SLES_538.19", USB_MODE, { 0xdeadbee1, 0x00000000, 0x00000000 }}, // Armored Core Nine Breaker PAL - skip failing case on binding a RPC server
 	{ "SLPS_254.08", USB_MODE, { 0xdeadbee1, 0x00000000, 0x00000000 }}, // Armored Core Nine Breaker NTSC J - skip failing case on binding a RPC server
-	{ NULL, 0, { 0, 0, 0 }}						    // terminater
+	{ "SLUS_202.30", ETH_MODE, { 0x00132d14, 0x10000018, 0x0c046744 }}, // Max Payne NTSC U - skip IOP reset before to exec demo elfs
+	{ NULL,                 0, { 0x00000000, 0x00000000, 0x00000000 }}  // terminater
 };
 
 static u32 NIScdtimeoutpattern[] = {

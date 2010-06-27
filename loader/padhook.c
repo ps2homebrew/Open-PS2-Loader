7d75b5af5409af5348c24c4df358fe4dd5b5ae7c
Michaël Jimenez <jimmikaelkael@wanadoo.fr>
2010-06-27 18:51:33 +0200

    EE core: added a ResetSPU() in LoadExecPS2 hook

--- loader/padhook.c
+++ loader/padhook.c
@@ -39,65 +39,6 @@ static u8 IGR_Stack[IGR_STACK_SIZE] __attribute__ ((aligned(16)));
 /* Extern symbol */
 extern void *_gp;
 
-// Reset SPU sound processor
-static void ResetSPU()
-{
-	u32 core;
-	volatile u16 *statx;
-
-	DIntr();
-	ee_kmode_enter();
-
-	// Stop SPU Dma Core 0
-	*SD_DMA_CHCR(0) &= ~SD_DMA_START;
-	*U16_REGISTER(0x1B0) = 0;
-
-	// Stop SPU Dma Core 1
-	*SD_DMA_CHCR(1) &= ~SD_DMA_START;
-	*U16_REGISTER(0x1B0 +  1024) = 0;
-
-	// Initialize SPU2
-	*U32_REGISTER(0x1404)  = 0xBF900000;
-	*U32_REGISTER(0x140C)  = 0xBF900800;
-	*U32_REGISTER(0x10F0) |= 0x80000;
-	*U32_REGISTER(0x1570) |= 8;
-	*U32_REGISTER(0x1014)  = 0x200B31E1;
-	*U32_REGISTER(0x1414)  = 0x200B31E1;
-
-	*SD_C_SPDIF_OUT = 0;
-	delay(1);
-	*SD_C_SPDIF_OUT = 0x8000;
-	delay(1);
-
-	*U32_REGISTER(0x10F0) |= 0xB0000;
-
-	for(core=0; core < 2; core++)
-	{
-		*U16_REGISTER(0x1B0)	= 0;
-		*SD_CORE_ATTR(core)		= 0;
-		delay(1);
-		*SD_CORE_ATTR(core)		= SD_SPU2_ON;
-
-		*SD_P_MVOLL(core)		= 0;
-		*SD_P_MVOLR(core)		= 0;
-		
-		statx = U16_REGISTER(0x344 + (core * 1024));
-
-		while(*statx & 0x7FF);
-		
-		*SD_A_KOFF_HI(core)		= 0xFFFF;
-		*SD_A_KOFF_LO(core)		= 0xFFFF; // Should probably only be 0xFF
-	}
-
-	*SD_S_PMON_HI(1)	= 0;
-	*SD_S_PMON_LO(1)	= 0;
-	*SD_S_NON_HI(1)		= 0;
-	*SD_S_NON_LO(1)		= 0;
-
-	ee_kmode_exit();
-	EIntr();
-}
-
 // Shutdown Dev9 hardware
 static void Shutdown_Dev9()
 {

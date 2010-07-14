fcb09d00fddc4fb9dd7d9dcf05e7e02b2eee9966
jimmikaelkael <jimmikaelkael@localhost>
2010-07-14 19:19:34 +0200

    fix for Kya: Dark Lineage US (SLUS_204.40)

--- loader/patches.c
+++ loader/patches.c
@@ -20,7 +20,7 @@ typedef struct {
 	game_patch_t patch;
 } patchlist_t;
 
-static patchlist_t patch_list[27] = {
+static patchlist_t patch_list[29] = {
 	{ "SLES_524.58", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness PAL - disable cdvd timeout stuff
 	{ "SLUS_206.66", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness NTSC U - disable cdvd timeout stuff
 	{ "SLPS_202.51", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Makai Senki Disgaea NTSC J - disable cdvd timeout stuff
@@ -47,6 +47,10 @@ static patchlist_t patch_list[27] = {
 	{ "SLES_537.77", HDD_MODE, { 0xdeadbee2, 0x00040000, 0x006cd6dc }}, // Prince of Persia: The Two Thrones PAL - slow down cdvd reads
 	{ "SLUS_202.30", ALL_MODE, { 0x00132d14, 0x10000018, 0x0c046744 }}, // Max Payne NTSC U - skip IOP reset before to exec demo elfs
 	{ "SLES_503.25", ALL_MODE, { 0x00132ce4, 0x10000018, 0x0c046744 }}, // Max Payne PAL - skip IOP reset before to exec demo elfs
+	{ "SLUS_204.40", ALL_MODE, { 0x0021bb00, 0x03e00008, 0x27bdff90 }}, // Kya: Dark Lineage NTSC U - disable game debug prints
+	{ "SLUS_204.40", ALL_MODE, { 0x0021bb04, 0x00000000, 0x3c0c0041 }}, // Kya: Dark Lineage NTSC U - disable game debug prints
+	//{ "SLES_514.73", ALL_MODE, { 0x0021bb00, 0x03e00008, 0x27bdff90 }}, // Kya: Dark Lineage PAL - disable game debug prints
+	//{ "SLES_514.73", ALL_MODE, { 0x0021bb04, 0x00000000, 0x3c0c0041 }}, // Kya: Dark Lineage PAL - disable game debug prints
 	//{ "SLUS_212.91", ALL_MODE, { 0x002efb28, 0x00000000, 0x10430009 }}, // Suikoden V NTSC U
 	//{ "SLES_540.87", ALL_MODE, { 0x002f0718, 0x00000000, 0x10430009 }}, // Suikoden V PAL
 	{ NULL,                 0, { 0x00000000, 0x00000000, 0x00000000 }}  // terminater

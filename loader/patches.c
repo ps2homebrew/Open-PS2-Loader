5c663e25513bdead7cc7af58adde7742ae559316
jimmikaelkael <jimmikaelkael@localhost>
2010-07-31 21:31:30 +0200

    fix for V-Rally 3 PAL (SLES_507.25)

--- loader/patches.c
+++ loader/patches.c
@@ -20,7 +20,7 @@ typedef struct {
 	game_patch_t patch;
 } patchlist_t;
 
-static patchlist_t patch_list[49] = {
+static patchlist_t patch_list[47] = {
 	{ "SLES_524.58", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness PAL - disable cdvd timeout stuff
 	{ "SLUS_206.66", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Disgaea Hour of Darkness NTSC U - disable cdvd timeout stuff
 	{ "SLPS_202.51", USB_MODE, { 0xdeadbee0, 0x00000000, 0x00000000 }}, // Makai Senki Disgaea NTSC J - disable cdvd timeout stuff
@@ -62,17 +62,11 @@ static patchlist_t patch_list[49] = {
 	{ "SLUS_202.30", ALL_MODE, { 0x00132d14, 0x10000018, 0x0c046744 }}, // Max Payne NTSC U - skip IOP reset before to exec demo elfs
 	{ "SLES_503.25", ALL_MODE, { 0x00132ce4, 0x10000018, 0x0c046744 }}, // Max Payne PAL - skip IOP reset before to exec demo elfs
 	{ "SLUS_204.40", ALL_MODE, { 0x0021bb00, 0x03e00008, 0x27bdff90 }}, // Kya: Dark Lineage NTSC U - disable game debug prints
-	{ "SLUS_204.40", ALL_MODE, { 0x0021bb04, 0x00000000, 0x3c0c0041 }}, // Kya: Dark Lineage NTSC U - disable game debug prints
 	{ "SLES_514.73", ALL_MODE, { 0x0021bd10, 0x03e00008, 0x27bdff90 }}, // Kya: Dark Lineage PAL - disable game debug prints
-	{ "SLES_514.73", ALL_MODE, { 0x0021bd14, 0x00000000, 0x3c0c0041 }}, // Kya: Dark Lineage PAL - disable game debug prints
 	{ "SLUS_204.96", ALL_MODE, { 0x00104900, 0x03e00008, 0x27bdff90 }}, // V-Rally 3 NTSC U - disable game debug prints
-	{ "SLUS_204.96", ALL_MODE, { 0x00104904, 0x00000000, 0x3c0c002a }}, // V-Rally 3 NTSC U - disable game debug prints
-	//{ "SLES_507.25", ALL_MODE, { 0x00104900, 0x03e00008, 0x27bdff90 }}, // V-Rally 3 PAL - disable game debug prints
-	//{ "SLES_507.25", ALL_MODE, { 0x00104904, 0x00000000, 0x3c0c002a }}, // V-Rally 3 PAL - disable game debug prints
+	{ "SLES_507.25", ALL_MODE, { 0x00104518, 0x03e00008, 0x27bdff70 }}, // V-Rally 3 PAL - disable game debug prints
 	{ "SLUS_201.99", ALL_MODE, { 0x0012a6d0, 0x24020001, 0x0c045e0a }}, // Shaun Palmer's Pro Snowboarder NTSC U
 	{ "SLUS_201.99", ALL_MODE, { 0x0013c55c, 0x10000012, 0x04400012 }}, // Shaun Palmer's Pro Snowboarder NTSC U
-	//{ "SLUS_212.91", ALL_MODE, { 0x002efb28, 0x00000000, 0x10430009 }}, // Suikoden V NTSC U
-	//{ "SLES_540.87", ALL_MODE, { 0x002f0718, 0x00000000, 0x10430009 }}, // Suikoden V PAL
 	{ NULL,                 0, { 0x00000000, 0x00000000, 0x00000000 }}  // terminater
 };
 

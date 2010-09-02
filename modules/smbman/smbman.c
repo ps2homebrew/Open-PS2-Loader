1a0ccf4f063db52f8853882a3d7f7f6d22c8798e
jimmikaelkael <jimmikaelkael@users.noreply.github.com>
2010-09-02 19:01:13 +0200

    smbman: added debug prints ability

--- modules/smbman/smbman.c
+++ modules/smbman/smbman.c
@@ -1,7 +1,7 @@
 /*
   Copyright 2009-2010, jimmikaelkael
   Licenced under Academic Free License version 3.0
-  Review OpenUsbLd README & LICENSE files for further details.
+  Review Open PS2 Loader README & LICENSE files for further details.
 */
 
 #include <loadcore.h>
@@ -10,15 +10,21 @@
 
 #include "smb_fio.h"
 #include "smb.h"
+#include "debug.h"
 
-#define MODNAME "smbman"
-IRX_ID(MODNAME, 2, 1);
+#define MODNAME 	"smbman"
+#define VER_MAJOR	2
+#define VER_MINOR	1
+
+IRX_ID(MODNAME, VER_MAJOR, VER_MINOR);
 
 struct irx_export_table _exp_smbman;
 
 //-------------------------------------------------------------------------
 int _start(int argc, char** argv)
 {
+	DPRINTF("%s version 0x%01x%02x start!\n", MODNAME, VER_MAJOR, VER_MINOR);
+
 	RegisterLibraryEntries(&_exp_smbman);
 
 	smb_initdev();

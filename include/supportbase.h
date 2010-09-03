8d8e962d37f81eb1537afa29038d5ffece49ce8d
izdubar <izdubar.bbln@gmail.com>
2010-09-03 23:22:33 +0200

    added rename and delete functionality for UL format (ul.cfg)
    (but rename is disabled for USB ul format for the moment, as it would not rename the file parts)

--- include/supportbase.h
+++ include/supportbase.h
@@ -18,5 +18,7 @@ int sbGetCompatibility(base_game_info_t* game, int mode);
 void sbSetCompatibility(base_game_info_t* game, int mode, int compatMode);
 void sbReadList(base_game_info_t **list, const char* prefix, const char* sep, int *fsize, int* gamecount);
 int sbPrepare(base_game_info_t* game, int mode, char* isoname, int size_cdvdman, void** cdvdman_irx, int* patchindex);
+void sbDelete(base_game_info_t **list, const char* prefix, const char* sep, int gamecount, int id);
+void sbRename(base_game_info_t **list, const char* prefix, const char* sep, int gamecount, int id, char* newname);
 
 #endif

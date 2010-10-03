dd6c2ef0dcb53a007dbe62a5e3e26f33600d5a8a
izdubar <izdubar.bbln@gmail.com>
2010-10-03 14:33:52 +0200

    fix DNAS settings bug

--- include/dialogs.h
+++ include/dialogs.h
@@ -5,8 +5,8 @@
 
 #define COMPAT_MODE_COUNT		8
 
-#define COMPAT_GAME				100
-#define COMPAT_MODE_BASE		101
+#define COMPAT_MODE_BASE		200
+#define COMPAT_GAME				101
 #define COMPAT_GAMEID			102
 #define COMPAT_SAVE				103
 #define COMPAT_TEST				104

8b11c93993092c3f840ee57ab0ae1414bb1072df
Izdubar Babylon <izdubar.bbln@gmail.com>
2011-02-06 10:48:00 +0100

    step 2, loose coupling

--- include/menusys.h
+++ include/menusys.h
@@ -3,46 +3,39 @@
 
 #include "include/texcache.h"
 
-/// Forward decls.
-struct menu_item_t;
-struct menu_list_t;
-
 /// a single submenu item
-struct submenu_item_t {
+typedef struct submenu_item {
 	/// Icon used for rendering of this item
 	int icon_id;
 	
 	/// item description
 	char *text;
 	
-	/// item description in localised form (used if value is not negative)
+	/// item description in localized form (used if value is not negative)
 	int text_id;
 	
 	/// item id
 	int id;
 	
-	/// do we have a pixmap at all? 0 no, 1 yes, 2 not sure yet
-	int pixmap_state[3];
-	
-	/// pointers to cache entries (used by cache code)
-	cache_entry_t *cache_entry_ref[3];
-};
+	int cache_id[3];
+	int cache_uid[3];
+} submenu_item_t;
 
-struct submenu_list_t {
-	struct submenu_item_t item;
+typedef struct submenu_list {
+	struct submenu_item item;
 	
-	struct submenu_list_t *prev, *next;
-};
+	struct submenu_list *prev, *next;
+} submenu_list_t;
 
-struct menu_hint_item_t {
+typedef struct menu_hint_item {
 	int icon_id;
 	int text_id;
 	
-	struct menu_hint_item_t *next;
-};
+	struct menu_hint_item *next;
+} menu_hint_item_t;
 
 /// a single menu item. Linked list impl (for the ease of rendering)
-struct menu_item_t {
+typedef struct menu_item {
 	/// Icon used for rendering of this item
 	int icon_id;
 	
@@ -55,43 +48,40 @@ struct menu_item_t {
 	void *userdata;
 	
 	/// submenu, selection and page start (only used in static mode)
-	struct submenu_list_t *submenu, *current, *pagestart;
+	struct submenu_list *submenu, *current, *pagestart;
 	
-	void (*refresh)(struct menu_item_t *self);
+	void (*refresh)(struct menu_item *self);
 
-	void (*execCross)(struct menu_item_t *self, int id);
+	void (*execCross)(struct menu_item *self, int id);
 	
-	void (*execTriangle)(struct menu_item_t *self, int id);
+	void (*execTriangle)(struct menu_item *self, int id);
 
-	void (*execCircle)(struct menu_item_t *self, int id);
+	void (*execCircle)(struct menu_item *self, int id);
 	
-	void (*execSquare)(struct menu_item_t *self, int id);
+	void (*execSquare)(struct menu_item *self, int id);
 	
 	/// hint list
-	struct menu_hint_item_t *hints;
-};
+	struct menu_hint_item *hints;
+} menu_item_t;
 
-struct menu_list_t {
-	struct menu_item_t *item;
+typedef struct menu_list {
+	struct menu_item *item;
 	
-	struct menu_list_t *prev, *next;
-};
+	struct menu_list *prev, *next;
+} menu_list_t;
 
 void menuInit();
 void menuEnd();
 
-void menuAppendItem(struct menu_item_t* item);
+void menuAppendItem(menu_item_t* item);
 
-struct submenu_list_t* submenuAppendItem(struct submenu_list_t** submenu, int icon_id, char *text, int id, int text_id);
-void submenuRemoveItem(struct submenu_list_t** submenu, int id);
-void submenuDestroy(struct submenu_list_t** submenu);
-void submenuSort(struct submenu_list_t** submenu);
+submenu_list_t* submenuAppendItem(submenu_list_t** submenu, int icon_id, char *text, int id, int text_id);
+void submenuRemoveItem(submenu_list_t** submenu, int id);
+void submenuDestroy(submenu_list_t** submenu);
+void submenuSort(submenu_list_t** submenu);
 
 /// Get the text of the item (considers localised version of the string)
-char *submenuItemGetText(struct submenu_item_t* it);
-
-/// callback from the defered pixmap load ended operation
-void submenuPixmapLoaded(icon_cache_t* cache, void *centry, int result);
+char *submenuItemGetText(submenu_item_t* it);
 
 void menuDrawStatic();
 
@@ -103,17 +93,15 @@ void menuNextPage();
 void menuPrevPage();
 void menuFirstPage();
 void menuLastPage();
-struct menu_item_t* menuGetCurrent();
+menu_item_t* menuGetCurrent();
 GSTEXTURE* menuGetCurrentArt();
-void menuItemExecButton(void (*execActionButton)(struct menu_item_t *self, int id));
+void menuItemExecButton(void (*execActionButton)(menu_item_t *self, int id));
 
 // Sets the selected item if it is found in the menu list
-void menuSetSelectedItem(struct menu_item_t* item);
+void menuSetSelectedItem(menu_item_t *item);
 
-void menuSetInactiveFrames(unsigned int frames);
-
-void menuAddHint(struct menu_item_t *menu, int text_id, int icon_id);
-void menuRemoveHints(struct menu_item_t *menu);
-void menuInitHints(struct menu_item_t* menu);
+void menuAddHint(menu_item_t *menu, int text_id, int icon_id);
+void menuRemoveHints(menu_item_t *menu);
+void menuInitHints(menu_item_t *menu);
 
 #endif
--- src/texcache.c
+++ src/texcache.c
@@ -1,235 +1,46 @@
 #include "include/texcache.h"
 #include "include/ioman.h"
 #include "include/gui.h"
-#include "include/menusys.h"
 #include "include/util.h"
-#include <stdio.h>
 
 static int frameID = 0;
+static int inactiveFrames = 0;
 
-// count of frames till the cache entry is considered to be old and not used any more (~2s)
-#define IDLE_FRAME_COUNT (100) 
+void cacheNextFrame(int inactives) {
+	++frameID;
+	inactiveFrames = inactives;
+}
 
 // io call to handle the loading of covers
 #define IO_MENU_LOAD_ART 5
+
 typedef struct {
-	icon_cache_t *cache;
-	cache_entry_t *entry;
-	item_list_t *list;
+	image_cache_t* cache;
+	cache_entry_t* entry;
+	item_list_t* list;
 	// only for comparison if the deferred action is still valid
-	struct submenu_item_t* item;
-	// id of the icon
-	int id;
-} load_art_request_t;
-
-// forward decl.
-static void cacheLoadArt(void* data);
-
-
-void cacheInit() {
-	ioRegisterHandler(IO_MENU_LOAD_ART, &cacheLoadArt);
-}
-
-void cacheEnd(void) {
-	// nothing to do...
-	// others have to destroy the cache via cacheDestroyCache
-}
-
-void cacheNextFrame(void) {
-	++frameID;
-}
-
-void cacheInitCache(icon_cache_t* cache, const char* prefix, int count) {
-	cache->count = count;
-	cache->prefix = prefix;
-	cache->cacheItemRemoved = NULL;
-	cache->cache = (cache_entry_t*)malloc(count * sizeof(cache_entry_t));
-	
-	int i;
-	
-	for (i = 0; i < cache->count; ++i) {
-		cache_entry_t *entry = &cache->cache[i];
-		
-		memset(entry, 0, sizeof(cache_entry_t));
-		entry->texture.Mem = NULL;
-		entry->texture.Vram = 0;
-		entry->qr = NULL;
-		entry->lastUsed = -1;
-		entry->item = NULL;
-	}
-}
-
-static void cacheClearItem(icon_cache_t* cache, cache_entry_t* item) {
-	// if there is a callback on remove, call it now
-	if (cache->cacheItemRemoved)
-		cache->cacheItemRemoved(cache, item);
-	
-	if (item->texture.Mem) {
-		free(item->texture.Mem);
-		item->texture.Mem = NULL;
-	}
-	
-	memset(item, 0, sizeof(cache_entry_t));
-	item->texture.Vram = 0;
-	item->qr = NULL;
-	item->lastUsed = -1;
-	item->item = NULL;
-}
-
-void cacheReleaseItem(icon_cache_t* cache, cache_entry_t* item) {
-	cacheClearItem(cache, item);
-}
-
-void cacheDestroyCache(icon_cache_t* cache) {
-	int i;
-	
-	for (i = 0; i < cache->count; ++i) {
-		cacheClearItem(cache, &cache->cache[i]);
-	}
-	
-	free(cache->cache);
-}
-
-void cacheRemoveItem(icon_cache_t* cache, struct submenu_item_t* item) {
-	int i;
-	
-	for (i = 0; i < cache->count; ++i) {
-		cache_entry_t *entry = &cache->cache[i];
-
-		if (entry->item == item) {
-			// Is it queued already? If so we have to clear it from queue
-			if (entry->qr) {
-				// invalidate the request
-				((load_art_request_t*)(entry->qr))->list = NULL;
-				// TODO: Locks via sema obj - this can cause a race condition!
-			}
-
-			cacheClearItem(cache, entry); 
-		}
-	}
-}
-
-/** find index of a candidate for removal (to replace when loading other items).
-* @param cache The cache to search
-* @param frameid The current frame id
-* @return negative number if there is no candidate, index of candidate if such exists
-*/ 
-static cache_entry_t * cacheFindCandidate(icon_cache_t* cache, struct submenu_item_t* item) {
-	int i;
-	
-	// first loop - scan for free slots only...
-	for (i = 0; i < cache->count; ++i) {
-		cache_entry_t *entry = &cache->cache[i];
-		
-		if (entry->item == item) // already cached, just return we found it
-			return entry;
-		
-		if (entry->lastUsed < 0)
-			return entry;
-	}
-	
-	cache_entry_t *torecycle = NULL;
-	int rtime = frameID;
-	
-	// second loop - scan for timeouted pixmaps...
-	for (i = 0; i < cache->count; ++i) {
-		cache_entry_t *entry = &cache->cache[i];
-		
-		// the entry was not used for a long time enough
-		if ((entry->qr == NULL) && ((frameID - entry->lastUsed) > IDLE_FRAME_COUNT)) {
-				// older than the prev. candidate?
-				if (entry->lastUsed < rtime) {
-					torecycle = entry;
-					rtime = entry->lastUsed;
-				}
-		}
-	}
-	
-	if (torecycle) {
-		// recycle
-		cacheClearItem(cache, torecycle);
-		// and return
-		return torecycle;
-	}
-	
-	return NULL;
-}
-
-int cacheGetEntry(icon_cache_t* cache, struct submenu_item_t* item, cache_entry_t **centry, item_list_t* handler) {
-	// valid input?
-	if (!centry || !cache || !centry || !handler)
-		return 0;
-	
-	// client already has a cache entry
-	if (*centry) {
-		// an item is retrieved. synchronise the lastUsed
-		// either still queued or texture not zero
-		if ((*centry)->qr != NULL || (*centry)->texture.Mem)
-			(*centry)->lastUsed = frameID;
-		
-		return ((*centry)->qr == NULL); // if queue requested, return false
-	}
-	
-	// no cache entry yet on client side, see what the situation is
-	cache_entry_t *entry = cacheFindCandidate(cache, item);
-		
-	if (!entry) // no candidate
-		return 0;
-	
-	// candidate found, link back
-	(*centry) = entry;
-	
-	if (entry->item == item) { // found a match - the item was cached but not linked!
-		if (entry->qr != NULL || entry->texture.Mem)
-			entry->lastUsed = frameID;
-		
-		return (entry->qr == NULL); // if queue requested, return false
-	}
-	
-	// found a candidate which was used for other menu item previously
-	// reuse the candidate
-	entry->lastUsed = frameID;
-	entry->item = item;
-	entry->qr = NULL;
-	
-	load_art_request_t* req = malloc(sizeof(load_art_request_t));
-	entry->qr = req;
-	
-	// not linking in the menu item. it can be invalid when the request comes to processing
-	// it will be searched for at the time the icon retrieval happens again
-	req->cache = cache;
-	req->entry = entry;
-	req->list = handler;
-	req->item = item;
-	req->id = item->id;
-	
-	ioPutRequest(IO_MENU_LOAD_ART, req);
-
-	return 0;
-}
+	int cacheUID;
+	// id of the item in the list
+	int itemId;
+} load_image_request_t;
 
 // Io handled action...
-static void cacheLoadArt(void* data) {
-	load_art_request_t* req = data;
-	
+static void cacheLoadImage(void* data) {
+	load_image_request_t* req = data;
+
 	// Safeguards...
-	if (!req)
+	if (!req || !req->entry || !req->cache)
 		return;
-	
+
 	item_list_t* handler = req->list;
-	if (!handler || !req->entry || !req->entry->item)
+	if (!handler)
 		return;
-	
-	if (!req->item)
-		return;
-	
+
 	// the cache entry was already reused!
-	if ((!req->entry->qr) || (req->item != req->entry->item))
+	if (/*(!req->entry->qr) || */(req->cacheUID != req->entry->UID)) {
+		LOG("cacheLoadImage(%d) - entry was re-used !! requested UID: %d entry UID: %d\n", req->cache->userId, req->cacheUID, req->entry->UID);
 		return;
-	
-	// different id for some reason
-	if ((req->id != req->item->id))
-		return;
+	}
 
 	// seems okay. we can proceed
 	GSTEXTURE* texture = &req->entry->texture;
@@ -237,27 +48,117 @@ static void cacheLoadArt(void* data) {
 		free(texture->Mem);
 		texture->Mem = NULL;
 	}
-	int result = handler->itemGetArt(handler->itemGetStartup(req->id), texture, req->cache->prefix, GS_PSM_CT24);
-	
-	// okay, the pixmap was loaded (or not if result is not null :))
-	// inform via deferred update about that
-	
-	struct gui_update_t *op = guiOpCreate(GUI_OP_PIXMAP_LOADED);
-	op->menu.menu = NULL;
-	op->menu.subMenu = NULL;
-	
-	op->pixmap.result = (result >= 0) ? 1 : 0;
-	op->pixmap.cache = req->cache;
-	op->pixmap.entry = req->entry;
-	
-	guiDeferUpdate(op);
-		
+
+	LOG("cacheLoadImage(%d) - loading image for UID: %d\n", req->cache->userId, req->cacheUID);
+
+	if (handler->itemGetArt(handler->itemGetStartup(req->itemId), texture, req->cache->prefix, GS_PSM_CT24) < 0)
+		req->entry->lastUsed = 0;
+	else
+		req->entry->lastUsed = frameID;
+
 	req->entry->qr = NULL;
-	
-	// free the request as it is not needed any more
+
 	free(req);
 }
 
-void cacheInformUsage(cache_entry_t *centry) {
-	centry->lastUsed = frameID;
+void cacheInit() {
+	ioRegisterHandler(IO_MENU_LOAD_ART, &cacheLoadImage);
+}
+
+void cacheEnd() {
+	// nothing to do... others have to destroy the cache via cacheDestroyCache
+}
+
+static void cacheClearItem(cache_entry_t* item, int freeTxt) {
+	if (freeTxt && item->texture.Mem)
+		free(item->texture.Mem);
+	
+	memset(item, 0, sizeof(cache_entry_t));
+	item->texture.Mem = NULL;
+	item->texture.Vram = 0;
+	item->qr = NULL;
+	item->lastUsed = -1;
+	item->UID = -1;
+}
+
+void cacheInitCache(image_cache_t* cache, int userId, const char* prefix, int count) {
+	cache->userId = userId;
+	cache->count = count;
+	cache->prefix = prefix;
+	cache->nextUID = 0;
+	cache->content = (cache_entry_t*) malloc(count * sizeof(cache_entry_t));
+
+	int i;
+	for (i = 0; i < count; ++i)
+		cacheClearItem(&cache->content[i], 0);
+}
+
+void cacheDestroyCache(image_cache_t* cache) {
+	int i;
+	for (i = 0; i < cache->count; ++i) {
+		cacheClearItem(&cache->content[i], 1);
+	}
+	
+	free(cache->content);
+}
+
+GSTEXTURE* cacheGetTexture(image_cache_t* cache, item_list_t* list, int* cacheId, int* UID, int itemId) {
+	if (*cacheId == -2) {
+		//LOG("cacheGetTexture(%d) - texture is null for UID: %d itemId: %d\n", cache->userId, *UID, itemId);
+		return NULL;
+	} else if (*cacheId != -1) {
+		cache_entry_t* entry = &cache->content[*cacheId];
+		if (entry->UID == *UID) {
+			if (entry->qr)
+				return NULL;
+			else if (entry->lastUsed == 0) {
+				LOG("cacheGetTexture(%d) - callback from cacheLoadImage, texture is null for UID: %d itemId: %d\n", cache->userId, *UID, itemId);
+				*cacheId = -2;
+				return NULL;
+			} else {
+				entry->lastUsed = frameID;
+				return &entry->texture;
+			}
+		}
+		LOG("cacheGetTexture(%d) - outdated UID: %d was at slot: %d entryUID: %d itemId: %d\n", cache->userId, *UID, *cacheId, entry->UID, itemId);
+
+		*cacheId = -1;
+	}
+
+	// under the cache pre-delay (to avoid filling cache while moving around)
+	if (inactiveFrames < list->delay)
+		return NULL;
+
+	cache_entry_t *currEntry, *oldestEntry = NULL;
+	int i, rtime = frameID;
+
+	for (i = 0; i < cache->count; i++) {
+		currEntry = &cache->content[i];
+		if ((!currEntry->qr) && (currEntry->lastUsed < rtime)) {
+			oldestEntry = currEntry;
+			rtime = currEntry->lastUsed;
+			*cacheId = i;
+		}
+	}
+
+	if (oldestEntry) {
+		load_image_request_t* req = malloc(sizeof(load_image_request_t));
+		req->cache = cache;
+		req->entry = oldestEntry;
+		req->list = list;
+		req->itemId = itemId;
+		req->cacheUID = cache->nextUID;
+
+		cacheClearItem(oldestEntry, 1);
+		oldestEntry->qr = req;
+		oldestEntry->UID = cache->nextUID;
+
+		*UID = cache->nextUID++;
+
+		LOG("cacheGetTexture(%d) - new request for UID: %d at slot: %d itemId: %d\n", req->cache->userId, *UID, *cacheId, itemId);
+
+		ioPutRequest(IO_MENU_LOAD_ART, req);
+	}
+
+	return NULL;
 }

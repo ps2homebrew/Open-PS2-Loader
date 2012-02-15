92b68ee77dead203ef3a9921624adfeb1cc5160a
Hominem te esse <hominem.te.esse@googlemail.com>
2012-02-15 22:13:25 +0100

    * reverted some changes from rev500, finally crazyc was correct (when he skipped one vmc in error, but still wrote the structure in IRX but with flag as inactive) * fixed bug when checking VMC (if VMC0 was in error and not VMC1 you didn't got any message) * prepare stuff for eth prefix feature (name size at 64 to include the prefix)

--- include/system.h
+++ include/system.h
@@ -1,6 +1,10 @@
 #ifndef __SYSTEM_H
 #define __SYSTEM_H
 
+#ifdef VMC
+#include "include/mcemu.h"
+#endif
+
 #define SYS_LOAD_MC_MODULES	0x01
 #define SYS_LOAD_PAD_MODULES	0x02
 
@@ -24,7 +28,7 @@ int sysLoadModuleBuffer(void *buffer, int size, int argc, char *argv);
 void sysApplyKernelPatches(void);
 int sysCheckMC(void);
 #ifdef VMC
-int sysCheckVMC(const char* prefix, const char* sep, char* name, int createSize);
+int sysCheckVMC(const char* prefix, const char* sep, char* name, int createSize, vmc_superblock_t* vmc_superblock);
 #endif
 
 #endif

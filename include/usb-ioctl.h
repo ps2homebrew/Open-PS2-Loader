1c40321f20329616dfea1445410beba1ca7c599d
SP193 <pirorin187@gmail.com>
2015-08-15 12:59:47 +0800

    Updated to support the new PS2SDK changes (SIF CMD buffer set, USBHDFSDV SIF CMD ID changed to 0), USBHDFSD IOCTL codes consolidated into usb-ioctl.h, standardized which "prefix" buffer to use within usbsupport.c (was unable to access game files when a prefix is set, due to the path getting incorrectly generated), removed cdrom_lseek64 as it is not implemented in the SONY CDVDMAN, changed cdrom_dclose to become cdrom_close since close() and dclose() are the same here and SONY does it that way too, simplified cdrom_lseek to be more tolerant to errors like the SONY original, adjusted CBT to apply to reading operations as well, adjusted CBT to be 5000us, adjusted speed limit for Accurate Reads to 2800KB/s (timed to the 36.864MHz IOP clock).

--- include/usb-ioctl.h
+++ include/usb-ioctl.h
@@ -1,7 +1,3 @@
-#ifndef _USBHDFSD_ADD_H
-#define _USBHDFSD_ADD_H
-
-//OPL-only codes
 #define USBHDFSD_IOCTL_GETSECTOR	0x1000	//jimmikaelkael: Ioctl request code => get file start sector
 #define USBHDFSD_IOCTL_GETCLUSTER	0x1001	//jimmikaelkael: Ioctl request code => get file start cluster
 #define USBHDFSD_IOCTL_GETSIZE		0x1002	//jimmikaelkael: Ioctl request code => get file size
@@ -10,5 +6,3 @@
 #define USBHDFSD_IOCTL_GETDATASTART	0x1005
 #define USBHDFSD_IOCTL_GETCLUSTERSIZE	0x1006
 #define USBHDFSD_IOCTL_CHECKCHAIN	0x1100	//polo: Ioctl request code => Check cluster chain
-
-#endif //_USBHDFSD_ADD_H

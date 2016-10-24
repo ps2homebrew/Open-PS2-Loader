0599f3cf89453d7a439716412981702cb5f045f9
doctorxyz <ghost@users.noreply.github.com>
2016-10-24 11:05:47 -0200

    Per-Game Cheat Engine fix finished. Phew!

--- include/gui.h
+++ include/gui.h
@@ -127,9 +127,6 @@ void guiShowNetCompatUpdate(void);
 void guiShowAbout();
 void guiShowConfig();
 void guiShowUIConfig();
-
-void guiShowCheatConfig();
-
 void guiShowNetConfig();
 
 /** Renders the given string on screen for the given function until it's io finishes 

461ee92fcdfd2cbb054d086a3936aeff1d8bb3d9
uyjulian <uyjulian@gmail.com>
2017-08-07 15:12:15 -0500

    ioman_add duplicate definition fix

--- modules/vmc/genvmc/ioman_add.h
+++ modules/vmc/genvmc/ioman_add.h
@@ -12,30 +12,6 @@ typedef struct _iop_ext_device
     struct _iop_ext_device_ops *ops;
 } iop_ext_device_t;
 
-typedef struct
-{
-    /*00*/ unsigned int mode;
-    /*04*/ unsigned int attr;
-    /*08*/ unsigned int size;
-    /*0c*/ unsigned char ctime[8];
-    /*14*/ unsigned char atime[8];
-    /*1c*/ unsigned char mtime[8];
-    /*24*/ unsigned int hisize;
-    /*28*/ unsigned int private_0; /* Number of subs (main) / subpart number (sub) */
-    /*2c*/ unsigned int private_1;
-    /*30*/ unsigned int private_2;
-    /*34*/ unsigned int private_3;
-    /*38*/ unsigned int private_4;
-    /*3c*/ unsigned int private_5; /* Sector start.  */
-} iox_stat_t;
-
-typedef struct
-{
-    iox_stat_t stat;
-    char name[256];
-    unsigned int unknown;
-} iox_dirent_t;
-
 typedef struct _iop_ext_device_ops
 {
     int (*init)(iop_device_t *);
@@ -68,8 +44,5 @@ typedef struct _iop_ext_device_ops
     int (*ioctl2)(iop_file_t *, int, void *, unsigned int, void *, unsigned int);
 
 } iop_ext_device_ops_t;
-
-int remove(const char *name);
-#define I_remove DECLARE_IMPORT(10, remove)
 
 #endif

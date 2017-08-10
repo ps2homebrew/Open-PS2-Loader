dbb8f01e0ee3ca5c75d46c2e402e774212885a8e
Rick Gaiser <rgaiser@gmail.com>
2017-08-10 17:10:25 +0200

    textures: Add bmp support (from gsKit)
    BMP support enables loading of 8bit textures. This can make textures use
    75% less VRAM.

--- include/textures.h
+++ include/textures.h
@@ -45,6 +45,7 @@ enum INTERNAL_TEXTURE {
 int texLookupInternalTexId(const char *name);
 int texPngLoad(GSTEXTURE *texture, const char *path, int texId, short psm);
 int texJpgLoad(GSTEXTURE *texture, const char *path, int texId, short psm);
+int texBmpLoad(GSTEXTURE *texture, const char *path, int texId, short psm);
 void texPrepare(GSTEXTURE *texture, short psm);
 int texDiscoverLoad(GSTEXTURE *texture, const char *path, int texId, short psm);
 

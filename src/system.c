ps2homebrew/Open-PS2-Loader

commit 1a79d7d9d732f694d41fef42aea1ebf0a720467a

Added fix for SOS: The Final Escape/Disaster Report/Zettai Zetsumei Toshi. This also includes new IOP patch functions.
The game suffered from a sound problem, which sometimes lead to instability.

There was a memo dated July 2000, which went along the lines of saying that the atick functions of modmidi and modhsyn should be called,
if related sequence data is stopepd and removed.

The explanation was that these modules only process sequence data when their atick functions are called.
If data is removed and their atick functions are not called before further callsto the library are made,
then the correct operation of the library cannot be guaranteed because the data would be dequeued.

So to correct this, the tick thread must be woken up after sequence data is dequeued.

src/system.c
@@ -334,18 +334,56 @@ void sysExecExit()
 #define CORE_IRX_DEBUG 0x20
 #define CORE_IRX_DECI2 0x40
 
-#ifdef VMC
-static unsigned int sendIrxKernelRAM(unsigned int modules, void *ModuleStorage, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx)
+typedef struct
+{
+    char *game;
+    char *mode;
+    void *module;
+    int *module_size;
+} patchlist_t;
+
+//Blank string for mode = all modes.
+static const patchlist_t iop_patch_list[] = {
+    {"SLUS_205.61", "", &iremsndpatch_irx, &size_iremsndpatch_irx},    //Disaster Report
+    {"SLES_513.01", "", &iremsndpatch_irx, &size_iremsndpatch_irx},    //SOS: The Final Escape
+    {"SLPS_251.13", "", &iremsndpatch_irx, &size_iremsndpatch_irx},    //Zettai Zetsumei Toshi
+    {NULL, NULL, NULL, NULL },  //Terminator
+};
+
+static unsigned int addIopPatch(const char *mode_str, const char *startup, irxptr_t *tab)
+{
+    const patchlist_t *p;
+    int i;
+
+    for (i = 0; iop_patch_list[i].game != NULL; i++)
+    {
+        p = &iop_patch_list[i];
+
+        if (!strcmp(p->game, startup) && (p->mode[0] == '\0' || !strcmp(p->mode, startup)))
+        {
+            tab->info = (*(p->module_size)) | SET_OPL_MOD_ID(OPL_MODULE_ID_IOP_PATCH);
+            tab->ptr = (void *)p->module;
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+static unsigned int sendIrxKernelRAM(const char *startup, const char *mode_str, unsigned int modules, void *ModuleStorage, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx)
 { // Send IOP modules that core must use to Kernel RAM
-#else
-static unsigned int sendIrxKernelRAM(unsigned int modules, void *ModuleStorage, int size_cdvdman_irx, void **cdvdman_irx)
-{ // Send IOP modules that core must use to Kernel RAM
-#endif
     irxtab_t *irxtable;
     irxptr_t *irxptr_tab;
     void *irxptr, *ioprp_image;
     int i, modcount;
     unsigned int curIrxSize, size_ioprp_image, total_size;
+
+    if (!strcmp(mode_str, "USB_MODE"))
+        modules |= CORE_IRX_USB;
+    else if (!strcmp(mode_str, "ETH_MODE"))
+        modules |= CORE_IRX_ETH | CORE_IRX_SMB;
+    else
+        modules |= CORE_IRX_HDD;
 
     irxtable = (irxtab_t *)ModuleStorage;
     irxptr_tab = (irxptr_t *)((unsigned char *)irxtable + sizeof(irxtab_t));
@@ -386,7 +424,7 @@ static unsigned int sendIrxKernelRAM(unsigned int modules, void *ModuleStorage, 
 #ifdef VMC
     if (modules & CORE_IRX_VMC) {
         irxptr_tab[modcount].info = size_mcemu_irx | SET_OPL_MOD_ID(OPL_MODULE_ID_MCEMU);
-        irxptr_tab[modcount++].ptr = (void *)mcemu_irx;
+        irxptr_tab[modcount++].ptr = (void *)&mcemu_irx;
     }
 #endif
 
@@ -419,6 +457,8 @@ static unsigned int sendIrxKernelRAM(unsigned int modules, void *ModuleStorage, 
     }
 #endif
 #endif
+
+    modcount += addIopPatch(mode_str, startup, &irxptr_tab[modcount]);
 
     irxtable->modules = irxptr_tab;
     irxtable->count = modcount;
@@ -564,7 +604,7 @@ static int ResetDECI2(void)
 }
 #endif
 
-void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, MCEMU int EnablePS2Logo, unsigned int compatflags)
+void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx, int EnablePS2Logo, unsigned int compatflags)
 {
     unsigned int modules, ModuleStorageSize;
     void *ModuleStorage;
@@ -607,13 +647,7 @@ void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, vo
     //Wipe the low user memory region, since this region might not be wiped after OPL's EE core is installed.
     memset((void *)0x00082000, 0, 0x00100000 - 0x00082000);
 
-    if (!strcmp(mode_str, "USB_MODE"))
-        modules = CORE_IRX_USB;
-    else if (!strcmp(mode_str, "ETH_MODE"))
-        modules = CORE_IRX_ETH | CORE_IRX_SMB;
-    else
-        modules = CORE_IRX_HDD;
-
+    modules = 0;
     ModuleStorage = (void *)((compatflags & COMPAT_MODE_7) ? OPL_MOD_STORAGE_HI : OPL_MOD_STORAGE);
 
 #ifdef __DECI2_DEBUG
@@ -624,12 +658,10 @@ void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, vo
 
 #ifdef VMC
     modules |= CORE_IRX_VMC;
-    LOG("SYSTEM LaunchLoaderElf loading modules with size_mcemu_irx = %d\n", size_mcemu_irx);
-    ModuleStorageSize = (sendIrxKernelRAM(modules, ModuleStorage, size_cdvdman_irx, cdvdman_irx, size_mcemu_irx, mcemu_irx) + 0x3F) & ~0x3F;
-#else
-    LOG("SYSTEM LaunchLoaderElf loading modules\n");
-    ModuleStorageSize = (sendIrxKernelRAM(modules, ModuleStorage, size_cdvdman_irx, cdvdman_irx) + 0x3F) & ~0x3F;
 #endif
+    LOG("SYSTEM LaunchLoaderElf loading modules\n");
+    ModuleStorageSize = (sendIrxKernelRAM(filename, mode_str, modules, ModuleStorage, size_cdvdman_irx, cdvdman_irx, size_mcemu_irx, mcemu_irx) + 0x3F) & ~0x3F;
+
     sprintf(ModStorageConfig, "%u %u", (unsigned int)ModuleStorage, (unsigned int)ModuleStorage + ModuleStorageSize);
 
     if (!gDisableDebug) {

Makefile
@@ -87,7 +87,8 @@ MISC_OBJS =	icon_sys_A.o icon_sys_J.o conf_theme_OPL.o
 IOP_OBJS =	iomanx.o filexio.o ps2fs.o usbd.o usbhdfsd.o usbhdfsdfsv.o \
 		ps2atad.o hdpro_atad.o poweroff.o ps2hdd.o xhdd.o genvmc.o hdldsvr.o \
 		ps2dev9.o smsutils.o ps2ip.o smap.o isofs.o nbns-iop.o \
-		httpclient-iop.o netman.o ps2ips.o
+		httpclient-iop.o netman.o ps2ips.o \
+		iremsndpatch.o
 
 EECORE_OBJS = ee_core.o ioprp.o util.o \
 		elfldr.o udnl.o imgdrv.o eesync.o \
@@ -282,6 +283,9 @@ clean:
 	$(MAKE) -C modules/iopcore/cdvdman USE_HDPRO=1 clean
 	echo " -cdvdfsv"
 	$(MAKE) -C modules/iopcore/cdvdfsv clean
+	echo "  -patches"
+	echo "   -iremsnd"
+	$(MAKE) -C modules/iopcore/patches/iremsndpatch clean
 	echo " -isofs"
 	$(MAKE) -C modules/isofs clean
 	echo " -usbhdfsdfsv"
@@ -429,6 +433,13 @@ modules/iopcore/cdvdfsv/cdvdfsv.irx: modules/iopcore/cdvdfsv
 
 $(EE_ASM_DIR)cdvdfsv.s: modules/iopcore/cdvdfsv/cdvdfsv.irx | $(EE_ASM_DIR)
 	$(BIN2S) $< $@ cdvdfsv_irx
+
+modules/iopcore/patches/iremsndpatch/iremsndpatch.irx: modules/iopcore/patches/iremsndpatch
+	echo " -iremsnd patch"
+	$(MAKE) -C $<
+
+$(EE_ASM_DIR)iremsndpatch.s: modules/iopcore/patches/iremsndpatch/iremsndpatch.irx | $(EE_ASM_DIR)
+	$(BIN2S) $< $@ iremsndpatch_irx
 
 modules/mcemu/usb_mcemu.irx: modules/mcemu
 	echo " -usb_mcemu"

ee_core/include/modules.h
@@ -23,6 +23,9 @@ enum OPL_MODULE_ID {
     OPL_MODULE_ID_DRVTIF,
     OPL_MODULE_ID_TIFINET,
 
+    //Special patches
+    OPL_MODULE_ID_IOP_PATCH,
+
     OPL_MODULE_ID_COUNT
 };
 

ee_core/src/patches.c
@@ -10,6 +10,8 @@
 
 #include "ee_core.h"
 #include "util.h"
+#include "modules.h"
+#include "modmgr.h"
 
 #define ALL_MODE -1
 
@@ -37,6 +39,7 @@ typedef struct
 #define PATCH_RNC_UYA 0x00398498
 #define PATCH_ZOMBIE_ZONE 0xEEE62525
 #define PATCH_DOT_HACK 0x0D074A37
+#define PATCH_SOS 0x30303030
 
 static const patchlist_t patch_list[] = {
     {"SLES_524.58", USB_MODE, {PATCH_GENERIC_NIS, 0x00000000, 0x00000000}},        // Disgaea Hour of Darkness PAL - disable cdvd timeout stuff
@@ -95,6 +98,9 @@ static const patchlist_t patch_list[] = {
     {"SLES_524.67", ALL_MODE, {PATCH_DOT_HACK, 0x00000000, 0x00000000}},           // .hack//Mutation PAL
     {"SLES_524.69", ALL_MODE, {PATCH_DOT_HACK, 0x00000000, 0x00000000}},           // .hack//Outbreak PAL
     {"SLES_524.68", ALL_MODE, {PATCH_DOT_HACK, 0x00000000, 0x00000000}},           // .hack//Quarantine PAL
+    {"SLUS_205.61", ALL_MODE, {PATCH_SOS, 0x00000001, 0x00000000}},                // Disaster Report
+    {"SLES_513.01", ALL_MODE, {PATCH_SOS, 0x00000002, 0x00000000}},                // SOS: The Final Escape
+    {"SLPS_251.13", ALL_MODE, {PATCH_SOS, 0x00000000, 0x00000000}},                // Zettai Zetsumei Toshi
     {NULL, 0, {0x00000000, 0x00000000, 0x00000000}}                                // terminater
 };
 
@@ -104,6 +110,7 @@ static const patchlist_t patch_list[] = {
 
 static int (*cdRead)(u32 lsn, u32 nsectors, void *buf, int *mode);
 static unsigned int g_delay_cycles;
+static int g_mode; //Patch may use this for anything.
 
 // Nippon Ichi Software games generic patch to disable cdvd timeout
 static void NIS_generic_patches(void)
@@ -456,6 +463,92 @@ int Skip_Videos_sceMpegIsEnd(void)
         return 0;
 }
 
+static int SOS_SifLoadModuleHook(const char *path, int arg_len, const char *args, int *modres,
+int fno)
+{
+    int (*_pSifLoadModule)(const char *path, int arg_len, const char *args, int *modres, int fno);
+    void *(*pSifAllocIopHeap)(int size);
+    int (*pSifFreeIopHeap)(void *addr);
+    int (*pSifLoadModuleBuffer)(void *ptr, int arg_len, const char *args);
+    void *iopmem;
+    SifDmaTransfer_t sifdma;
+    int dma_id, ret, ret2;
+    void *iremsndpatch_irx;
+    unsigned int iremsndpatch_irx_size;
+
+    switch(g_mode)
+    {
+        case 0: //NTSC-J
+            _pSifLoadModule = (void*)0x001d0680;
+            pSifAllocIopHeap = (void*)0x001cfc30;
+            pSifFreeIopHeap = (void*)0x001cfd20;
+            pSifLoadModuleBuffer = (void*)0x001d0640;
+            break;
+        case 1: //NTSC-U/C
+            _pSifLoadModule = (void*)0x001d0580;
+            pSifAllocIopHeap = (void*)0x001cfb30;
+            pSifFreeIopHeap = (void*)0x001cfc20;
+            pSifLoadModuleBuffer = (void*)0x001d0540;
+            break;
+        case 2: //PAL
+            _pSifLoadModule = (void*)0x001d11c0;
+            pSifAllocIopHeap = (void*)0x001d0770;
+            pSifFreeIopHeap = (void*)0x001d0860;
+            pSifLoadModuleBuffer = (void*)0x001d1180;
+            break;
+        default:
+            _pSifLoadModule = NULL;
+            pSifAllocIopHeap = NULL;
+            pSifFreeIopHeap = NULL;
+            pSifLoadModuleBuffer = NULL;
+    }
+
+    ret = _pSifLoadModule(path, arg_len, args, modres, fno);
+
+    if((ret >= 0) && (_pSifLoadModule != NULL) && (_strcmp(path, "cdrom0:\\IOP\\IREMSND.IRX;1") == 0))
+    {
+        GetOPLModInfo(OPL_MODULE_ID_IOP_PATCH, &iremsndpatch_irx, &iremsndpatch_irx_size);
+
+        iopmem = pSifAllocIopHeap(iremsndpatch_irx_size);
+        if(iopmem != NULL)
+        {
+            sifdma.src = iremsndpatch_irx;
+            sifdma.dest = iopmem;
+            sifdma.size = iremsndpatch_irx_size;
+            sifdma.attr = 0;
+            do {
+                dma_id = SifSetDma(&sifdma, 1);
+            } while (!dma_id);
+
+            do {
+                ret2 = pSifLoadModuleBuffer(iopmem, 0, NULL);
+            } while (ret2 < 0);
+
+            pSifFreeIopHeap(iopmem);
+        }
+    }
+
+    return ret;
+}
+
+static void SOSPatch(int region)
+{
+    g_mode = region;
+
+    switch(region)
+    {    // JAL SOS_SifLoadModuleHook - replace call to _SifLoadModule.
+        case 0: //NTSC-J
+            _sw(JAL((u32)&SOS_SifLoadModuleHook), 0x001d08b4);
+            break;
+        case 1: //NTSC-U/C
+            _sw(JAL((u32)&SOS_SifLoadModuleHook), 0x001d07b4);
+            break;
+        case 2: //PAL
+            _sw(JAL((u32)&SOS_SifLoadModuleHook), 0x001d13f4);
+            break;
+    }
+}
+
 void apply_patches(const char *path)
 {
     const patchlist_t *p;
@@ -490,6 +583,8 @@ void apply_patches(const char *path)
                     break;
                 case PATCH_DOT_HACK:
                     DotHack_patches(path);
+                case PATCH_SOS:
+                    SOSPatch(p->patch.val);
                     break;
                 default: // Single-value patches
                     if (_lw(p->patch.addr) == p->patch.check)

include/extern_irx.h
@@ -58,6 +58,9 @@ extern int size_ioptrap_irx;
 extern void *isofs_irx;
 extern int size_isofs_irx;
 
+extern void *iremsndpatch_irx;
+extern int size_iremsndpatch_irx;
+
 extern void *mcman_irx;
 extern int size_mcman_irx;
 

include/system.h
@@ -17,13 +17,7 @@ void sysReset(int modload_mask);
 void sysExecExit();
 void sysPowerOff(void);
 
-#ifdef VMC
-#define MCEMU int size_mcemu_irx, void **mcemu_irx,
-#else
-#define MCEMU
-#endif
-
-void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, MCEMU int EnablePS2Logo, unsigned int compatflags);
+void sysLaunchLoaderElf(char *filename, char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx, int EnablePS2Logo, unsigned int compatflags);
 
 int sysExecElf(char *path);
 int sysLoadModuleBuffer(void *buffer, int size, int argc, char *argv);

modules/iopcore/patches/iremsndpatch/Makefile
@@ -1,0 +1,14 @@
+IOP_BIN  = iremsndpatch.irx
+IOP_OBJS = main.o imports.o
+
+IOP_INCS +=
+IOP_CFLAGS += -Os -Wall -fno-builtin
+IOP_LDFLAGS += -s
+
+all: $(IOP_BIN)
+
+clean:
+	-rm -f $(IOP_OBJS) $(IOP_BIN)
+
+include $(PS2SDK)/Defs.make
+include ../../../Rules.make

modules/iopcore/patches/iremsndpatch/imports.lst
@@ -1,0 +1,8 @@
+loadcore_IMPORTS_start
+I_GetLoadcoreInternalData
+loadcore_IMPORTS_end
+
+thbase_IMPORTS_start
+I_WakeupThread
+thbase_IMPORTS_end
+

commit a18983225fa245b64b1c7b83f6b41e12b791e103

Fixed bug introduced in 1a79d7d: wrong pointer used for VMC.

src/system.c
@@ -421,7 +421,7 @@ static unsigned int sendIrxKernelRAM(const char *startup, const char *mode_str, 
 #ifdef VMC
     if (modules & CORE_IRX_VMC) {
         irxptr_tab[modcount].info = size_mcemu_irx | SET_OPL_MOD_ID(OPL_MODULE_ID_MCEMU);
-        irxptr_tab[modcount++].ptr = (void *)&mcemu_irx;
+        irxptr_tab[modcount++].ptr = (void *)mcemu_irx;
     }
 #endif
 
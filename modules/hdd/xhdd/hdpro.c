f574ea3fc2c992b302b8464462d64fb22cb2c99e
SP193 <pirorin187@gmail.com>
2018-08-26 13:54:46 +0800

    ATA: Disable the device's write cache for in-game VMC support.

--- modules/hdd/xhdd/hdpro.c
+++ modules/hdd/xhdd/hdpro.c
@@ -9,3 +9,9 @@ int hdproata_device_set_transfer_mode(int device, int type, int mode)
 {
     return 0;
 }
+
+/* Set features - enable/disable write cache.  */
+int hdproata_device_set_write_cache(int device, int enable)
+{   //As the start and finish functions are not exported, disable the write cache within the in-game ATAD instead.
+    return 0;
+}
--- modules/hdd/xhdd/xhdd.h
+++ modules/hdd/xhdd/xhdd.h
@@ -1,2 +1,4 @@
 int ata_device_set_transfer_mode(int device, int type, int mode);
+int ata_device_set_write_cache(int device, int enable);
 int hdproata_device_set_transfer_mode(int device, int type, int mode);
+int hdproata_device_set_write_cache(int device, int enable);

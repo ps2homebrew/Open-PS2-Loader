d2dbd9f51523c9a3522a7c9e88f90d91ea020577
sp193 <sp193@users.noreply.github.com>
2018-09-29 23:19:02 +0800

    CDVDFSV: reduced stack size for RPC thread 0 and 1, from 0x1900 to 0x500 and 0x800 to 0x400 respectively. These RPCs do not call functions that trigger callbacks, so it is not necessary to preserve their original stack sizes.

--- modules/iopcore/cdvdfsv/cdvdfsv.c
+++ modules/iopcore/cdvdfsv/cdvdfsv.c
@@ -259,7 +259,7 @@ static void cdvdfsv_startrpcthreads(void)
     thread_param.attr = TH_C;
     thread_param.option = 0xABCD8001;
     thread_param.thread = (void *)cdvdfsv_rpc1_th;
-    thread_param.stacksize = 0x1900;
+    thread_param.stacksize = 0x500; //Original: 0x1900. Its operations probably won't need so much space, since its functions will never trigger a callback.
     thread_param.priority = 0x51;
 
     rpc1_thread_id = CreateThread(&thread_param);
@@ -277,7 +277,7 @@ static void cdvdfsv_startrpcthreads(void)
     thread_param.attr = TH_C;
     thread_param.option = 0xABCD8000;
     thread_param.thread = (void *)cdvdfsv_rpc0_th;
-    thread_param.stacksize = 0x800;
+    thread_param.stacksize = 0x400; //Original: 0x800. Its operations probably won't need so much space, since its functions will never trigger a callback.
     thread_param.priority = 0x51;
 
     rpc0_thread_id = CreateThread(&thread_param);

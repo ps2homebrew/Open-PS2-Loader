a8cfeeaaa742bec33067a386ab74d9ce546dd98e
Bignaux Ronan <ronan@aimao.org>
2021-08-28 15:26:20 +0200

    Fix and Update lang_French.lng, and typo in opl.c

--- src/opl.c
+++ src/opl.c
@@ -1448,7 +1448,7 @@ void handleLwnbdSrv()
     // prepare for lwnbd, display screen with info
     guiRenderTextScreen(_l(_STR_STARTINGNBD));
     if (loadLwnbdSvr() == 0) {
-        snprintf(temp, sizeof(temp), "%s\nIP: %d.%d.%d.%d %s", _l(_STR_RUNNINGNBD),
+        snprintf(temp, sizeof(temp), "%s, IP: %d.%d.%d.%d %s", _l(_STR_RUNNINGNBD),
                  ps2_ip[0], ps2_ip[1], ps2_ip[2], ps2_ip[3], ps2_ip_use_dhcp ? "DHCP" : "");
         guiMsgBox(temp, 0, NULL);
     } else

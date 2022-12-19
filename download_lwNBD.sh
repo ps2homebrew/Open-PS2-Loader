6dba9d354b7357c304305d49aebbfc59106f48a4
Bignaux Ronan <ronan@aimao.org>
2022-12-19 15:25:07 +0100

    lwNBD: switch to main branch

--- download_lwNBD.sh
+++ download_lwNBD.sh
@@ -3,7 +3,7 @@
 ## Download lwNBD
 REPO_URL="https://github.com/bignaux/lwNBD.git"
 REPO_FOLDER="modules/network/lwNBD"
-BRANCH_NAME="opl"
+BRANCH_NAME="main"
 if test ! -d "$REPO_FOLDER"; then
   git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER" || exit 1
 else

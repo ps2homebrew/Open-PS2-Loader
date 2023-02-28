6b3a518e3bff9a8457cba6d37f407d6afa595fe5
Bignaux Ronan <ronan@aimao.org>
2023-02-28 19:36:38 +0100

    fix download_lwNBD.sh

--- download_lwNBD.sh
+++ download_lwNBD.sh
@@ -3,9 +3,8 @@
 ## Download lwNBD
 REPO_URL="https://github.com/bignaux/lwNBD.git"
 REPO_FOLDER="modules/network/lwNBD"
-BRANCH_NAME="b74564e7222b04b211c7c6bd04b3c4ed25be4e0b"
+COMMIT="b74564e7222b04b211c7c6bd04b3c4ed25be4e0b"
 if test ! -d "$REPO_FOLDER"; then
-  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER" || exit 1
-else
-  (cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" && cd - )|| exit 1
+  git clone $REPO_URL "$REPO_FOLDER" || { exit 1; }
+  (cd $REPO_FOLDER && git checkout $COMMIT && cd -) || { exit 1; }
 fi

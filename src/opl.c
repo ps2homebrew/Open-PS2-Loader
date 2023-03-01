6f16ccd08854fb6c6c9cc143ec04ca0ddc4795cf
Bignaux Ronan <ronan@aimao.org>
2023-03-01 10:57:43 +0100

    lwNBD: use official repository

--- src/opl.c
+++ src/opl.c
@@ -1414,9 +1414,17 @@ int oplUpdateGameCompatSingle(int id, item_list_t *support, config_set_t *config
 // ----------------------------------------------------------
 // -------------------- NBD SRV Support ---------------------
 // ----------------------------------------------------------
+
+
 static int loadLwnbdSvr(void)
 {
     int ret, padStatus;
+    struct lwnbd_config
+    {
+        char defaultexport[32];
+        uint8_t readonly;
+    };
+    struct lwnbd_config config;
 
     // deint audio lib while nbd server is running
     audioEnd();
@@ -1433,11 +1441,21 @@ static int loadLwnbdSvr(void)
     unloadPads();
     // sysReset(0); // usefull ? printf doesn't work with it.
 
+    /* compat stuff for user not providing name export (useless when there was only one export) */
+    ret = strlen(gExportName);
+    if (ret == 0)
+        strcpy(config.defaultexport, "hdd0");
+    else
+        strcpy(config.defaultexport, gExportName);
+
+    config.readonly = !gEnableWrite;
+
+    // see gETHStartMode, gNetworkStartup ? this is slow, so if we don't have to do it (like debug build).
     ret = ethLoadInitModules();
     if (ret == 0) {
-        ret = sysLoadModuleBuffer(&ps2atad_irx, size_ps2atad_irx, 0, NULL);
+        ret = sysLoadModuleBuffer(&ps2atad_irx, size_ps2atad_irx, 0, NULL); /* gHDDStartMode ? */
         if (ret >= 0) {
-            ret = sysLoadModuleBuffer(&lwnbdsvr_irx, size_lwnbdsvr_irx, 4, (char *)&gExportName);
+            ret = sysLoadModuleBuffer(&lwnbdsvr_irx, size_lwnbdsvr_irx, sizeof(config), (char *)&config);
             if (ret >= 0)
                 ret = 0;
         }
@@ -1490,8 +1508,7 @@ void handleLwnbdSrv()
     // prepare for lwnbd, display screen with info
     guiRenderTextScreen(_l(_STR_STARTINGNBD));
     if (loadLwnbdSvr() == 0) {
-        snprintf(temp, sizeof(temp), "%s IP: %d.%d.%d.%d %s", _l(_STR_RUNNINGNBD),
-                 ps2_ip[0], ps2_ip[1], ps2_ip[2], ps2_ip[3], ps2_ip_use_dhcp ? "DHCP" : "");
+        snprintf(temp, sizeof(temp), "%s", _l(_STR_RUNNINGNBD));
         guiMsgBox(temp, 0, NULL);
     } else
         guiMsgBox(_l(_STR_STARTFAILNBD), 0, NULL);

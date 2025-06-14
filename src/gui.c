08c8f3850a432651c644f106e3a4e0f52002b5a1
峰哥 <362053534@qq.com>
2025-06-14 22:50:38 +0800

    Optimize code

--- src/gui.c
+++ src/gui.c
@@ -1006,13 +1006,13 @@ static void guiHandleOp(struct gui_update_t *item)
             break;
 
         case GUI_OP_APPEND_MENU:
-            result = submenuAppendItem(item->menu.subMenu, item->submenu.icon_id,
-                                       item->submenu.text, item->submenu.id, item->submenu.text_id);
-
+            result = submenuAppendItem(item->menu.subMenu, item->submenu.icon_id, item->submenu.text, item->submenu.id, item->submenu.text_id);
             if (!item->menu.menu->submenu) { // first subitem in list
                 item->menu.menu->submenu = result;
-                item->menu.menu->current = result;
-                item->menu.menu->pagestart = result;
+                if (!item->submenu.selected) {
+                    item->menu.menu->current = result;
+                    item->menu.menu->pagestart = result;
+                }
             }
             if (item->submenu.selected) { // remember last played game feature
                 item->menu.menu->current = result;
